fdf5888ddd2512aaf0254bfb3c7c0bcd460b6879
Aarondomin <aarondomin012@gmail.com>
2026-01-15 09:28:10 +0100

    creación de maven

---
+++ UD3/ej1/diagramas/src/main/java/diagrama/App.java
@@ -0,0 +1,13 @@
+package diagrama;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
