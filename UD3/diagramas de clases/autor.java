2960a038afe143abb9ab7f0a7884312cecd70edd
Aarondomin <aarondomin012@gmail.com>
2026-01-29 09:39:29 +0100

    actualizacion do exercicio de diagrama de clases

---
+++ UD3/diagramas de clases/autor.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class autor {
+    
+    public class autor{
+        public String nombre;
+        public String nacionalidad;
+        public String fechaNacimiento;
+        public Arraylist<libro> libros;
+
+    public autor(String nombre, String nacionalidad, String fechaNacimiento){
+
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        this.fechaNacimiento = fechaNacimiento;
+        this.libros = new Arraylist<>();
+
+        }
+
+    }
+
+}
